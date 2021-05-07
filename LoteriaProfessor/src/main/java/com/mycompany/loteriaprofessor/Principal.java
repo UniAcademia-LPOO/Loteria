cc5714e7bfda6fea821dbf5649d84a06ca86678a
Jv <joaodcyrillo@gmail.com>
2021-05-07 18:34:57 -0300

    Construção classe Time

---
+++ LoteriaProfessor/src/main/java/com/mycompany/loteriaprofessor/Principal.java
@@ -0,0 +1,9 @@
+package com.mycompany.loteriaprofessor;
+
+
+
+public class Principal {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ LoteriaProfessor/src/main/java/com/mycompany/loteriaprofessor/model/Time.java
@@ -0,0 +1,23 @@
+package com.mycompany.loteriaprofessor.model;
+
+
+public class Time {
+    private String time;
+
+    public Time(String time) {
+        this.time = time;
+    }
+
+    public Time() {
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+    
+    
+}
